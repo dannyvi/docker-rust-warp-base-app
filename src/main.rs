b8de61682958f153ace0029521dd53e30551d506
dannyvi <dannyvis@icloud.com>
2022-11-05 12:24:51 +0800

    config github actions

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use warp::{Filter};
 #[tokio::main]
 async fn main() {
 
-    let route = warp::any().map(|| "Hello");
+    let route = warp::any().map(|| "Hello from github build");
     warp::serve(route).run(([0, 0, 0, 0], 8000)).await;
 }
